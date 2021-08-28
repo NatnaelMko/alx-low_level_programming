81bae0d32422e35d80e10bb7718a01a9392ae624
NatnaelMko <natnaelmekonnent@gmail.com>
2021-08-28 23:59:17 +0300

    Sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
 /**
- * 
  * main - entry point
  *
- * Description: My 
+ * Description: My
  *
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 
 {
-	int i;
-	double d;
-	char c;
-	long l;
-	short s;
+int i;
+double d;
+char c;
 
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of double is: %lu.\n", (unsigned long)sizeof(d));
+printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+printf("The size of double is: %lu.\n", (unsigned long)sizeof(d));
+printf("The size of char is: %lu. \n", (unsigned long)sizeof(c));
 
-	return (0);	
+return (0);
 }

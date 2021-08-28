d4517036c2718a6e8f07edfb3be18d11b92da831
NatnaelMko <natnaelmekonnent@gmail.com>
2021-08-29 00:14:19 +0300

    Sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,6 @@ long int li;
 long long int lli;
 
 printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-printf("The size of double is: %lu.\n", (unsigned long)sizeof(d));
 printf("The size of char is: %lu.\n", (unsigned long)sizeof(c));
 printf("The size of long int is: %lu.\n", (unsigned long)sizeof(li));
 printf("The size of long long int is: %lu.\n", (unsigned long)sizeof(lli));

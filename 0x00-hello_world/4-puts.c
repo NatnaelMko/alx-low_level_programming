9670169e3198dca978860bae19c0627c859d12e9
NatnaelMko <natnaelmekonnent@gmail.com>
2021-08-28 21:56:35 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void)
-/*
- * Hello My Firest description
- * My second description
- */
+	/*
+ 	 * Hello My Firest description
+ 	 * My second description
+ 	 */
 
 {
 puts("\"Programming is like building a multilingual puzzle");

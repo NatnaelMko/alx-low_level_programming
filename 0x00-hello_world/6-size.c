b3490f238013c44db92c68fc8cd9fa5fdd28a0ce
NatnaelMko <natnaelmekonnent@gmail.com>
2021-08-28 23:33:42 +0300

    Sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * 
+ * main: entry point
+ *
+ * Description: My third c programming file.
+ * 
+ */
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+	long l;
+	short s;
+
+	printf("The size of an int is: %lu.\n". (unsigned long)sizeof(i));
+	printf("The size of double is: %lu.\n". (unsigned long)sizeof(d));
+
+	return (0);	
+}

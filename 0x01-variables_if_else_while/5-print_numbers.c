ac2c7665f1d19b29def2bee55d89025977951096
NatnaelMko <natnaelmekonnent@gmail.com>
2021-08-30 23:46:11 +0300

    Exceptional

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Descritpion: Alphabets 2
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch = 0;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		printf(ch\n);
+	}
+
+	return (0);
+}

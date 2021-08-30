c6a1634fde5cae5ff4a797fae08535efc92aa159
NatnaelMko <natnaelmekonnent@gmail.com>
2021-08-30 22:28:36 +0300

    Exceptional

--- 0x01-variables_if_else_while/4-print_alphabet.c
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -13,15 +13,10 @@
 int main(void)
 {
 	char ch = 'a';
-	char CH = 'A';
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch < e && ch < q && ch <= 'z'; ch++)
 	{
 		putchar(ch);
-	}
-	for (CH = 'A'; CH <= 'Z'; CH++)
-	{
-		putchar(CH);
 	}
 		putchar('\n');
 

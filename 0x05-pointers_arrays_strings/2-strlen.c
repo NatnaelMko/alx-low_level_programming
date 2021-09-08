53767c007c6b93f41df5e43a0ed513e90989ad5d
NatnaelMko <natnaelmekonnent@gmail.com>
2021-09-09 01:17:21 +0300

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include "string.h"
+
+/**
+ * _strlen - check the code
+ * @s: here is a string with number of characheters.
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+int len;
+
+len = strlen(s);
+printf("%d\n", len);
+
+return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,8 @@ void print_alphabet_x10(void);
 void print_alphabet(void);
 int _islower(int c);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+
+
 
 #endif

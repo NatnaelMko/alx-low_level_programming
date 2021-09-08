a8763e6a90dc967d82131197e0a699d22d99aade
NatnaelMko <natnaelmekonnent@gmail.com>
2021-09-09 00:46:08 +0300

    Swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,12 +5,15 @@
  * swap_int - swap values of two integers
  *
  * Descritpion: b parameter*
- *  
+ * @a: a is 1st parameter
+ * @b: b is 2nd parameter
  * Return: Always 0.
  */
 
 void swap_int(int *a, int *b)
 {
+/* Description: a */
+/* Description: b */
 
 *a += *b;
 *b = *a - *b;

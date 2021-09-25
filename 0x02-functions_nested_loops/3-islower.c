c7bdd54537942d6e5d6e8ebf4e8521c6ddbdfa3b
NatnaelMko <natnaelmekonnent@gmail.com>
2021-09-25 16:02:57 +0300

    Files Present fixed

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: 0
+ */
+
+int _islower(c)
+{
+
+if ( islower(c) )
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

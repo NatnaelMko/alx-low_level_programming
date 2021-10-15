a64c8b8160caed84a7fa0ca99517b1afdfeaffd0
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-15 15:27:57 +0300

    islower_Betty_quick_fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,15 @@
 
 /**
  * _islower - checks for lowercase character
- *
+ * 
+ * Description: Islowr
+ * 
  * Return: 0
  */
 
-
+/**
+ * Description: it is int
+ */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
@@ -20,7 +24,7 @@ return (0);
 }
 }
 
-/** 
+/**
 *int main(void)
 * {
 * char c = 'D';

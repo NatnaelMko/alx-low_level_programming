7508a3c1148923bea902cb301c1a74289ab4aa13
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-15 16:49:35 +0300

    sign task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - this is the function
+ *
+ * @n: this is a variable
+ *
+ * Description: This is the longest description
+ *
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+    if (n > 0)
+    {
+        printf("+");
+        return (1);
+    } else if (n = 0)
+    {
+        printf("0");
+        return (0);
+    } else
+    {
+        printf("-1");
+        return (-1);
+    }
+}

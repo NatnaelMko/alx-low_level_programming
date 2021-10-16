d60fe2934e9a7c12c7e7644532dc82b93798a5f0
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-16 13:57:23 +0300

    24hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - this is the function
+ *
+ * @n: this is a variable
+ *
+ * Description: This is the longest description
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+int x = 0;
+int a = 0, b = 0, c = 0, d = 0;
+
+while(x < 1440)
+{
+    _putchar(a + '0');
+    _putchar(b + '0');
+    _putchar(':');
+    _putchar(c + '0');
+    _putchar(d + '0');
+
+    d++;
+    if (d > 9)
+    {
+        d = 0;
+        c++;
+    }
+    if (c > 5)
+    {
+        c = 0;
+        b++;
+    }
+    if (b > 9 )
+    {
+        b = 0;
+        a++;
+    }
+    x++;
+}
+}

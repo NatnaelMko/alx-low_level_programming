91230aad7b501151e3c59e2a401d30b4d6586ded
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-16 11:27:59 +0300

    abs function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - this is the function
+ *
+ * @n: this is a variable
+ *
+ * Description: This is the longest description
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+return n;
+}
+else
+{
+return (-1 * n);
+}
+}

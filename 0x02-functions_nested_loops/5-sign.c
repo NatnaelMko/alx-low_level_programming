f85a435c0179b19db62f81c9f9948f73dd2fc325
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-16 10:26:19 +0300

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,17 +15,21 @@ int print_sign(int n)
 {
     if (n > 0)
     {
-         
+        _putchar('0');
         return (+1);
     } else if (n == 0)
     {
-        
+        _putchar('0');
         return (0);
     } else
     {
-        
+        _putchar('-');
         return (-1);
     }
 }
 
-
+int main(void)
+{
+    int n = 98;
+    print_sign(n);
+}

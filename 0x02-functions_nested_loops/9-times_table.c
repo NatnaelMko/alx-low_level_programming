e759d6774d0702eb1fba907d250414402a65301f
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-18 12:18:01 +0300

    times_tabel

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - this is the function
+ *
+ * Description: This is the longest description
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+for (int i = 0; i < 10; i++)
+{
+for (int n = 0; n < 10; i++)
+{
+_putchar(i*n);
+}
+_putchar('\n')
+}
+}

66ebf46165a5bae7a74e268789460fad1eddf75a
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-18 19:23:58 +0300

    Simplify the code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,6 +13,22 @@
 void print_to_98(int n)
 {
 int i;
+
+if (n > 98)
+{
+for (i = n; i >= 98; i--)
+{
+printf ("%d", i);
+
+if (i != 98)
+{
+printf (", ");
+}
+}
+}
+
+else
+{
 for (i = n; i <= 98; i++)
 {
 printf ("%d", i);
@@ -21,15 +37,6 @@ if (i != 98)
 {
 printf (", ");
 }
-}
-
-for (i = n; i > 98; i--)
-{
-printf ("%d", i);
-
-if (i != 98)
-{
-printf (", ");
 }
 }
 _putchar('\n');

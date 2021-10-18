40a17ec870d76d4c65a4c6d9841e90b7cb3be4d7
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-18 18:03:29 +0300

    Add

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,8 @@
 
 void print_to_98(int n)
 {
-for (int i = n; i <= 98; i++)
+int i;
+for (i = n; i <= 98; i++)
 {
 if (i < 10)
 {
@@ -26,5 +27,6 @@ _putchar (i / 10 + '0');
 _putchar (i % 10 + '0');
 _putchar (',');
 _putchar (' ');
+}
 }
 }

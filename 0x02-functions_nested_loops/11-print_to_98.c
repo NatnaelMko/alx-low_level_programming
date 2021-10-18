378f37a76bf6a078ec272bb7cd4187697fc34569
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-18 18:37:10 +0300

    Add

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,8 +18,6 @@ for (i = n; i <= 98; i++)
 if (i < 10)
 {
 _putchar (i + '0');
-_putchar (',');
-_putchar (' ');
 }
 else if (i == 98)
 {
@@ -32,18 +30,26 @@ else
 {
 _putchar (i / 10 + '0');
 _putchar (i % 10 + '0');
+}
+
+if (i != 98)
+{
 _putchar (',');
 _putchar (' ');
 }
 }
 
-for (i = n; i > 98; i--)
+for (i = n; i >= 98; i--)
 {
 if (i > 98 && i < 100)
 {
 _putchar (i / 10 + '0');
 _putchar (i % 10 + '0');
-_putchar (',');
+}
+else if (i == 98)
+{
+_putchar (i / 10 + '0');
+_putchar (i % 10 + '0');
 _putchar (' ');
 }
 else
@@ -51,6 +57,9 @@ else
 _putchar (i / 100 + '0');
 _putchar (i / 10 % 10 + '0');
 _putchar (i % 10 + '0');
+}
+if (i != 98)
+{
 _putchar (',');
 _putchar (' ');
 }

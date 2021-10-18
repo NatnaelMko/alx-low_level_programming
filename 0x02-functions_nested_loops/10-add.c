c7f0192b197a26b68ed0361d20b6ba6e30acdbad
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-18 17:03:48 +0300

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - function
+ *
+ * Description: Long description of function
+ *
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+int z;
+z = a + b;
+return z;
+}

1ce268d91d2a25648e3b0ceaaf9582bb8a91d45b
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-19 12:55:28 +0300

    Hotfix_times table for n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,27 +3,21 @@
 
 /**
  * print_times_table - this is the function
- *
  * @n: the first variable
- *
  * Description: This is the full description
- *
  * Return: 0
  */
 
 void print_times_table(int n)
 {
 int x, y, z;
-
 if (n > 15 || n < 0)
 return;
-
 for (x = 0; x <= n; x++)
 {
 for (y = 0; y <= n; y++)
 {
 z = x * y;
-
 if (z > 99)
 {
 _putchar (z / 100 + '0');
@@ -46,7 +40,6 @@ else
 {
 _putchar (z + '0');
 }
-
 if (y != n)
 {
 _putchar (',');

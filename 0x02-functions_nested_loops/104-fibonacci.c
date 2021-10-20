fe0ffbf41727c82aba52b4a2e061b15a36d829a5
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-20 18:25:48 +0300

    98 fibonacci number lists

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - a function to print the first 98 fibonacci numbers
+ * Description: refer the description above
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+long a = 1;
+long b = 2;
+long sum = 0;
+
+for (i = 1; i <= 98; i++)
+{
+
+sum = a;
+a = b;
+b += sum;
+
+if (i < 98)
+{
+printf ("%lu, ", b);
+}
+if (i = 98)
+printf ("%lu", b);
+}
+printf("\n");
+return (0);
+}

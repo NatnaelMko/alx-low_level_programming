7dd4fb51cfacc56c2ebd5c458b97c35b2dfe4199
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-20 17:27:01 +0300

    Even sum function

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - a function to print even numbers in fibonacci series up to 4,000,000
+ * Description: Read the above
+ * Return: 0;
+ */
+
+int main(void)
+{
+int even_sum = 0;
+long a = 1;
+long b = 1;
+
+while(b < 4000000)
+{
+if (b % 2 == 0)
+{
+even_sum += b;
+}
+
+long sum = a;
+a = b;
+b += sum;
+}
+printf("\n");
+return (0);
+}

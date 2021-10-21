6fcdddafaaa3416f420c07923e695d9c8b9ff93e
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-21 09:14:49 +0300

    Test

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,19 +14,19 @@ long a = 1;
 long b = 2;
 long sum = 0;
 
-for (i = 1; i <= 98; i++)
+for (i = 1; i <= 10; i++)
 {
 sum = a;
 a = b;
 b += sum;
 
-if (i < 98)
+if (i < 10)
 {
 printf("%li, ", sum);
 }
-if (i == 98)
+if (i == 10)
 {
-printf("%li,...", sum);
+printf("%li", sum);
 }
 }
 printf("\n");

3144cc7cbd9f3c5e62110b0af9b84c567013a358
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-21 18:41:50 +0300

    multiply function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - a function
+ * @a: variable 1
+ * @b: variable 2
+ * Description: a function to multiply two intigers
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+int z;
+z = a * b;
+
+printf("%d", z);
+return (0);
+}

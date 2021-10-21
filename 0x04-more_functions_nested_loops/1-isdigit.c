319f05b446be46d9767f11c9253fd57ee111efc8
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-21 16:00:55 +0300

    isdigit function

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _isdigit - a function to identify a digit from letters
+ * @c: a variable
+ * Description: refer the above
+ * Return: 0
+ */
+
 int _isdigit(int c)
 {
 if (c >= 0 && c <= 9)

2115bfcec63e81603deb1a58cc07bef591424e4f
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-22 16:46:38 +0300

    insert more putchar function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@
 void more_numbers(void)
 {
 int i;
-int j;
 for (i = 0; i <= 10; i++)
 {
+int j;
 for (j = 0; j < 15; j++)
 {
 if (j > 9)
@@ -22,10 +22,11 @@ _putchar(j % 10 + '0');
 }
 else
 {
-printf("%d", j);
+_putchar(j + '0');
 }
+printf("\n");
+
 }
 printf("\n");
 }
-_putchar('\n');
 }

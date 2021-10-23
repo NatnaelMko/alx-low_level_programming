0e09fb6188b0aeb55c2cfffd0d820f8ecd5958a4
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-23 13:23:32 +0300

    fix on if else statement

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - name of the function
+ * Description: The “Fizz-Buzz test” is an interview question designed to help filter out the 99.5% of programming job candidates who can’t seem to program their way out of a wet paper bag.
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+
+else
+{
+printf("%d ", i);
+}
+}
+return (0);
+}

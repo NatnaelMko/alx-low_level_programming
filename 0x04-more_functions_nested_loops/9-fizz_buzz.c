e09a03f7652518c9f231f17031268afac2a31c8e
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-23 13:32:47 +0300

    shift print a new line statement

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,13 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+
+else if (i % 3 == 0)
 {
 printf("Fizz ");
 }
@@ -20,16 +26,11 @@ else if (i % 5 == 0)
 printf("Buzz ");
 }
 
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz ");
-}
-
 else
 {
 printf("%d ", i);
+}
+}
 printf("\n");
-}
-}
 return (0);
 }

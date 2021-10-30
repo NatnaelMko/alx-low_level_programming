a8b343fe68a387a007962f4b48d333b5d731c4c5
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-10-30 09:28:27 +0300

    prime factor task

--- 0x04-more_functions_nested_loops/100-main.c
+++ 0x04-more_functions_nested_loops/100-main.c
@@ -5,11 +5,11 @@
  *
  * Return: Always 0.
  */
-int main() 
+int main(void) 
 {
-/*is_prime(12319);*/
+is_prime(1231952);
 /*largerst_prime_factor(144015);*/
 
-largerst_prime_factor(1270);
+largerst_prime_factor(1231952);
 return (0);
 }
--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,39 +1,25 @@
 #include <stdio.h>
-#include "main.h"
+#include <math.h>
+
 /**
- * Description: a function that finds and pirngs the largest prime factor of the number
- * @n: a parameter
+ * main - main block
+ * Description: Find and print the largest prime factor of the number
+ * 612852475143
  * Return: 0
  */
+int main(void)
+{
+	int c;
+	long num = 612852475143;
 
-int is_prime(int n)
-{
-int i;
+	for (c = (int) sqrt(num); c > 2; c++)
+	{
+		if (num % c == 0)
+		{
+			printf("%d\n", c);
+			break;
+		}
+	}
 
-for (i = 2; i < n; i++)
-{
-if (n % i == 0)
-{
-return (0);
-}
-}
-/* printf("%d, is prime number", n);*/
-return (1);
-}
-
-int largerst_prime_factor(int n)
-{
-int i;
-for (i = n; i > 1; i--)
-{
-if (is_prime(i) == 1)
-{
-if (n % i == 0)
-{
-printf("%d", i);
-return(i);
-}
-}
-}
-return(0);
+	return (0);
 }

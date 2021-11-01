6552380991adc3cb1d34cf68b26bf3830e293a24
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-11-01 17:35:39 +0300

    fix comments for printf memebers

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,14 +3,12 @@
 /**
  * main - causes an infinite loop
  * Description: this main function to print 2 lines
- * @\\o/\n: this is the print symboles
  * Return: 0
  */
-
 int main(void)
 {
 printf("Infinite loop incoming :(\n");
-
+/*Print first*/
 /**
  * int i;
  * i = 0;
@@ -19,6 +17,8 @@ printf("Infinite loop incoming :(\n");
  * putchar(i);
  * }
  */
+
 printf("Infinite loop avoided! \\o/\n");
+/*print Last*/
 return (0);
 }

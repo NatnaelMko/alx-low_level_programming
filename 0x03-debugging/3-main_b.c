ea443547786fb119c8751e548f3007d31c49f397
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-11-05 15:05:34 +0300

    fix if condition

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -14,8 +14,8 @@ int day;
 int year;
 
 month = 2;
-day = 29;
-year = 2001;
+day = 12;
+year = 2019;
 
 printf("Date: %02d/%02d/%04d\n", month, day, year);
 
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,17 +14,13 @@ void print_remaining_days(int month, int day, int year)
 {
 if ((year % 4 == 0 || year % 400 == 0) || (year % 100 == 0))
 {
-if (month >= 1 && day <= 31)
-{
-day--;
-}
 
 printf("Day of the year: %d\n", day + 1);
 printf("Remaining days: %d\n", 366 - day - 1);
 }
 else
 {
-if ((month > 12 && month < 1) || (day > 31 && day < 1) || (month == 2 && day > 28))
+if (month == 2 && day >= 60)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }

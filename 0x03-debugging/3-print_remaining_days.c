92dcc71518158a6eb970b4803ab1a4f7a9662731
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-11-05 15:07:25 +0300

    delete

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,8 +15,8 @@ void print_remaining_days(int month, int day, int year)
 if ((year % 4 == 0 || year % 400 == 0) || (year % 100 == 0))
 {
 
-printf("Day of the year: %d\n", day + 1);
-printf("Remaining days: %d\n", 366 - day - 1);
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {

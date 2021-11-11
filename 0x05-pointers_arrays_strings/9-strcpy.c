811e3bb2325542de9c9baacb946619c65369b4f6
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-11-11 09:01:25 +0300

    strcpy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: destination parameter
+ * @src: source of string parameter
+ * Description: short
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+long count;    
+for (count = 0; src[count] != '\0'; count++)
+{
+dest[count] = src[count];
+}
+dest[count] = src[count];
+printf("%s\n", dest);
+return (dest);
+}

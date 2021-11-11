494e9a5ceb8118a9caaddb7dc746807d967763f3
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-11-11 06:21:12 +0300

    print array function first push

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array -  function to print n elements of an array
+ * @a: array
+ * @n: number of elements of an array
+ * Description: SHort description
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n && a[n] != '\0'; i++)
+{
+printf("%d, ", a[i]);
+}
+printf("\n");
+}

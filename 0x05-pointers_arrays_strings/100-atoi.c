8d9c495070bca45dfe38c6ff0b439cd1fe6c1e45
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-11-12 08:43:47 +0300

    fix function to consider - sign before the number

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,6 +13,7 @@ int _atoi(char *s)
 int count;
 int n = 0;
 int j;
+int i;
 int is_positive = 1;
 
 for (count = 0; s[count] != '\0'; count++)
@@ -24,18 +25,33 @@ n = (n * 10) + (s[count] - '0');
 
 for (j = count; j >= 0; j--)
 {
+
 if (n > 0 && n < 10 && s[j] == 45)
+{
+for (i = 0; j >= 0; j--, i++)
+{
+;
+}
+
+if (i % 2 != 0)
 {
 is_positive = 0;
 }
+else
+{
+continue;
+}
 
 }
+}
+
 if (s[count + 1] > 47 && s[count + 1]  < 58)
 {
 continue;
 }
 else
 {
+printf("hello\n");
 break;
 }
 

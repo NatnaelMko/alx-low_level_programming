42692b0e8454a61c76d9ccbccc58766e2404a723
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-11-12 06:46:10 +0300

    fix to print the nearest - sign and not print numbers that comes after string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,24 +12,38 @@ int _atoi(char *s)
 {
 int count;
 int n = 0;
+int j;
 int is_positive = 1;
 
 for (count = 0; s[count] != '\0'; count++)
 {
-   
+
 if (s[count] > 47 && s[count] < 58)
 {
 n = (n * 10) + (s[count] - '0');
 
-if (n > 0 && n < 10 && s[count - 1] == 45) 
+for (j = count; j >= 0; j--)
+{
+if (n > 0 && n < 10 && s[j] == 45)
 {
 is_positive = 0;
 }
 
 }
+if (s[count + 1] > 47 && s[count + 1]  < 58)
+{
+continue;
+}
+else
+{
+printf("hello\n");
+break;
 }
 
-if (!is_positive) 
+}
+}
+
+if (!is_positive)
 {
 n *= -1;
 }

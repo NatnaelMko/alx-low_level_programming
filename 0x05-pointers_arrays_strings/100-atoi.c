430f239fda05edef4f63e6caf0ac98431b4a1301
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-11-12 10:18:38 +0300

    refactorization of the code

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,56 +11,33 @@
 int _atoi(char *s)
 {
 int count;
+int n_count = 0;
 int n = 0;
-int j;
-int i;
-int is_positive = 1;
 
 for (count = 0; s[count] != '\0'; count++)
 {
+if (s[count] == 45 && n == 0)
+{
+n_count++;
+}
 
 if (s[count] > 47 && s[count] < 58)
 {
+
+if (n_count % 2 == 0)
+{
 n = (n * 10) + (s[count] - '0');
-
-for (j = count; j >= 0; j--)
-{
-
-if (n > 0 && n < 10 && s[j] == 45)
-{
-for (i = 0; j >= 0; j--, i++)
-{
-;
-}
-
-if (i % 2 != 0)
-{
-is_positive = 0;
 }
 else
 {
-continue;
-}
-
+n = (n * 10) - (s[count] - '0');
 }
 }
 
-if (s[count + 1] > 47 && s[count + 1]  < 58)
-{
-continue;
-}
-else
+else if (n != 0)
 {
 break;
 }
-
 }
-}
-
-if (!is_positive)
-{
-n *= -1;
-}
-
 return (n);
 }

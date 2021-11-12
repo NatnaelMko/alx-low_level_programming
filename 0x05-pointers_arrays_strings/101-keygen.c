bef13ac0b77b97720331da52060957af6d809090
Natnael <88345461+NatnaelMko@users.noreply.github.com>
2021-11-12 11:18:34 +0300

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a random password generator
+ * Description: find from the above
+ * Return: 0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int myrand;
+	int count;
+	int total;
+
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
+
+	return (0);
+}

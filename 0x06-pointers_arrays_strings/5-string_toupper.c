e58a29de1cc23998997ce96cd17117cf60737a17
NatnaelMko <natnaelmekonnent@gmail.com>
2021-11-18 12:23:56 +0300

    replace n by 0

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,7 +5,7 @@ char *string_toupper(char *s)
 {
 int i;
 
-for (i = 0; s[i] != '\n'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 
 if (s[i] >= 97 && s[i] <= 122)
@@ -13,7 +13,6 @@ if (s[i] >= 97 && s[i] <= 122)
 s[i] = s[i] - 32;
 }
 
-
 }
-return(s);
+return (s);
 }

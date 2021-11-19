ab3685a8a096ae37d9bf479ff7e706278d1d9afc
NatnaelMko <natnaelmekonnent@gmail.com>
2021-11-19 17:10:01 +0300

    Rot13 function

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+char *rot13(char *s)
+{
+int i;
+int j;
+int x[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+'H', 'I', 'J', 'K', 'L', 'M', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j <= 78; j++)
+{
+if (s[i] == x[j])
+{
+s[i] = x[j + 13];
+break;
+}
+
+}
+
+}
+return (s);
+}

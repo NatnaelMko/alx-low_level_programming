9b258c7cda22ad5e310472b6b7a2397c0880616a
NatnaelMko <natnaelmekonnent@gmail.com>
2021-11-20 12:08:35 +0300

    print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+#include <limits.h>
+
+/**
+ * print_number - print an integer
+ * @n: this is a parameter
+ * Description: a function to print an integer using _putchar
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+int i;
+int power;
+int num;
+
+num = n;
+
+if (num < 0)
+{
+num *= -1;
+_putchar('-');
+}
+
+power = 1;
+
+while (num >= 10)
+{
+power *= 10;
+num /= 10;
+}
+
+num = n;
+num = num < 0 ? num * -1 : num;
+
+for (i = power; i >= 1; i /= 10)
+{
+int temp;
+
+temp = num / i;
+_putchar(temp + '0');
+num %= i;
+
+if (num % i == 0)
+{
+temp = 0;
+}
+
+}
+
+}

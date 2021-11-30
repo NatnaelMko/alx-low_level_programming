0b5b23491d48b4051fa3804af80d42119031fa61
NatnaelMko <natnaelmekonnent@gmail.com>
2021-11-30 09:30:44 +0300

    strspn function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -20,12 +20,8 @@ if (*s == c)
 return (s);
 }
 
-else if (*(s + 1) == c)
-{
-return (s + 1);
-}
 s++;
 }
 
-return (s + 1);
+return (s);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,26 +11,26 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-int i;
-int count;
+unsigned int i;
+unsigned int j;
+unsigned int c = 0;
 
 for (i = 0; s[i] != '\0'; i++)
 {
-;
-}
-for (count = 0; accept[count] != '\0'; count++)
+for (j = 0; accept[j] != '\0'; j++)
 {
-;
+if (s[i] == accept[j])
+{
+c++;
+break;
 }
 
-if (accept[count] == s[i])
+else if (accept[j + 1] == '\0')
 {
-;
+return (c);
 }
 
-else if (accept[count] != s[i])
-{
-return (0);
 }
-return (count + 1);
+}
+return (c);
 }
